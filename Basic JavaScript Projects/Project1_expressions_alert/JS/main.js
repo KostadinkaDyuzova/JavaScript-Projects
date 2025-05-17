e99f47d3c97ff2c840fa63264d42e8ba611d524f
KostadinkaDyuzova <dyuzova.kostadinka@gmail.com>
2025-05-18 00:00:59 +0100

    First attempts

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,30 @@
+alert("Hello,Sunshine!");
+window.alert("Hello,World!");
+document.write('Hello, I am!');
+var A ="This is a string";
+document.writeln(A);
+var I='My name is Kostadinka';
+window.alert(I);
+var E= "He said that, \"I don't like this!\"";
+document.writeln(E);
+var C="\"I' m your new student,"
++"and I would like to introduce myself.\"";
+document.writeln(C);
+var CC="I am" + " Kostadinka Dyuzova";
+document.writeln(CC);
+var Family = "Lovchalievi", Dad="Diyan", Mom="Kostadinka", Daughter="Valeriya", Son="Kristiyan";
+document.writeln(Dad);
+
+document.writeln(5+10);
+function My_First_Function() {
+    var str = "This is the button text";
+    document.getElementById("Button_Text").innerHTML = str;
+}
+
+//creating two variables
+var Sent1= "This is the beginning of the string";
+var Sent2 = " and this is the end of the string";
+
+document.writeln(Sent1+Sent2);
+
+document.writeln(2*56);
---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,6 @@
+//function that uses the +=operator
+function myFunction() {
+    var sentence = "I am new in JavaScript"; //variable
+    sentence+= " and enjoy a lot.";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
